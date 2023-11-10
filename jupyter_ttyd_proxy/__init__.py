72c2558af45c000201bcfdb750b4b34492536630
Colin Torney <colin.j.torney@gmail.com>
2023-11-10 17:39:20 +0000

    options for ttyd

--- jupyter_ttyd_proxy/__init__.py
+++ jupyter_ttyd_proxy/__init__.py
@@ -3,19 +3,29 @@ Return config on servers to start for jupyter-ttyd-proxy
 
 """
 import os
+import shutil
 
 
 def setup_ttyd():
 
     def _get_command(port):
-        return ['ttyd', '-W', '-p', '{}'.format(port), 'bash']
+        
+        executable = shutil.which('ttyd')
+        if executable is None:
+            raise FileNotFoundError('Cannot find ttyd executable')
+
+        executable = shutil.which('tmux')
+        if executable is None:
+            return ['ttyd', '-W', '-p', '{}'.format(port), '-t', 'enableSixel=true', '--once', 'bash']
+        else:
+            return ['ttyd', '-W', '-p', '{}'.format(port), '-t', 'enableSixel=true', '--once', 'tmux', 'new-session', '-A', '-s', 'main']
 
     return {
         'command': _get_command,
         'absolute_url': False,
         'environment': {},
         'launcher_entry': {
-            'title': 'jupyter-ttyd-proxy',
+            'title': 'Terminal (ttyd)',
             'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'jupyter-ttyd-proxy.svg')
         }
     }
