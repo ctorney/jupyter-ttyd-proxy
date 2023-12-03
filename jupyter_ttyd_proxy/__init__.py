44cccd9250e90d0969f05c3d20a06fb43fd29664
ctorney <colin.j.torney@gmail.com>
2023-12-03 09:02:23 +0000

    now using zellij

--- jupyter_ttyd_proxy/__init__.py
+++ jupyter_ttyd_proxy/__init__.py
@@ -14,18 +14,18 @@ def setup_ttyd():
         if executable is None:
             raise FileNotFoundError('Cannot find ttyd executable')
 
-        executable = shutil.which('tmux')
+        executable = shutil.which('zellij')
         if executable is None:
-            return ['ttyd', '-W', '-p', '{}'.format(port), '-t', 'enableSixel=true', '-t', 'disableReconnect=true', 'bash']
+            return ['ttyd', '-W', '-p', '{}'.format(port), "-t", "fontSize=14", "-t", "enableSixel=true", "-t", "fontFamily=HackNerdFontMono", "-t", "titleFixed=jupyterhub",  '-t', 'enableSixel=true', '-t', 'disableReconnect=true', 'bash']
         else:
-            return ['ttyd', '-W', '-p', '{}'.format(port), '-t', 'enableSixel=true', '-t', 'disableReconnect=true', 'tmux', 'new-session', '-A', '-s', 'main']
+            return ['ttyd', '-W', '-p', '{}'.format(port), "-t", "fontSize=14", "-t", "enableSixel=true", "-t", "fontFamily=HackNerdFontMono", "-t", "titleFixed=Zellij",  '-t', 'enableSixel=true', '-t', 'disableReconnect=true', "zellij", "attach", "-c", "'jupyterhub'"]
 
     return {
         'command': _get_command,
         'absolute_url': False,
         'environment': {},
         'launcher_entry': {
-            'title': 'Terminal (ttyd)',
-            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'jupyter-ttyd-proxy.svg')
+            'title': 'Zellij (ttyd)',
+            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'jupyter-ttyd-proxy.png')
         }
     }
